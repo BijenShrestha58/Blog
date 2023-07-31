dc78091dc040b34063daca0f2e83a1422bf208e4
Abindra <abindrashakya.123@gmail.com>
2023-07-31 17:46:40 +0545

    Revert: Revert method for user register

--- backend/src/modules/user/user.controller.js
+++ backend/src/modules/user/user.controller.js
@@ -5,7 +5,7 @@ const SECRET_KEY = require('../../config/keys');
 const jwt = require('jsonwebtoken');
 
 const userRegister = async (req,res) => {
-    const {username, password}= req.body;
+    const {firstname, lastname, email,username, password}= req.body;
     const user = await userModel.findOne({
         username
     });
@@ -16,9 +16,19 @@ const userRegister = async (req,res) => {
         })
     }else{
         const encryptedPassword =await encryptPassword(password)
-        await userModel.create({
-            username,password :encryptedPassword
-        })
+        const newUser = new userModel({
+            username,
+            password:encryptedPassword
+          });
+        await newUser.save();
+
+        const userDetails = new userdetailsModel({
+            user: newUser._id, 
+            firstname,
+            lastname,
+            email
+          });
+        await userDetails.save();
         return res.status(401).send({
             data: null,
             message: 'User created successfully'
