eaed38ec9d796978723b6b0cd477217cf1274d36
Abindra <abindrashakya.123@gmail.com>
2023-10-18 20:05:13 +0545

    Feature:Database connected final

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const router = express.Router();
 // const axios = require('axios')
-const cors = require('cors');
-const blogRoutes = require('./modules/blog/blog.routes');
-const userRoutes = require('./modules/user/user.routes');
-const commentRoutes = require('./modules/comment/comment.routes');
+const cors = require("cors");
+const blogRoutes = require("./modules/blog/blog.routes");
+const userRoutes = require("./modules/user/user.routes");
+const commentRoutes = require("./modules/comment/comment.routes");
 
 // const todoController = require('./modules/todo/todo.controller')
 app.listen(8001, () => {
-    console.log(`API is listening on port 8001`);
+  console.log(`API is listening on port 8001`);
 });
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use('/todo',todoRoutes)
-app.use('/user',userRoutes)
-app.use('/blog',blogRoutes)
-app.use('/comment',commentRoutes)
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+app.use("/comment", commentRoutes);
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.9okskgq.mongodb.net/");
-const database = mongoose.connection; 
-database.on('error', (error) => {
-    console.log(error)
+mongoose.connect("mongodb+srv://admin:admin@cluster0.i1jb4fx.mongodb.net/");
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log(error);
 });
 
-
-database.once('connected', () => {
-    console.log('Database Connected');
+database.once("connected", () => {
+  console.log("Database Connected");
 });
 
 app.use("/", router);
--- backend/src/modules/blog/blog.controller.js
+++ backend/src/modules/blog/blog.controller.js
@@ -1,95 +1,104 @@
-const userSchema = require('../user/user.schema');
-const blogSchema= require('./blog.schema');
-const blogtypeschema = require('./blogtypes.schema');
+const userSchema = require("../user/user.schema");
+const blogSchema = require("./blog.schema");
+const blogtypesSchema = require("./blogtypes.schema");
 
-const getAllBlog=async(req,res)=>{
-    console.log('hello')
-    return res.send({
-        data: await blogSchema.find({}).populate('user','username _id')
-    })
-}
-const getById = async(req,res)=>{
-    return res.send({
-        data:await blogSchema.find({_id: req.params.id}).populate('user','username _id')
-    })
-}
+const getAllBlog = async (req, res) => {
+  console.log("hello");
+  return res.send({
+    data: await blogSchema.find({}).populate("user", "username _id"),
+  });
+};
+const getById = async (req, res) => {
+  return res.send({
+    data: await blogSchema
+      .find({ _id: req.params.id })
+      .populate("user", "username _id"),
+  });
+};
 
-const getByUser = async(req,res)=>{
-    const username = req.params.username;
-    const user = await userSchema.findOne({ username});
-    if (!user) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    return res.send({
-        data:await blogSchema.find({user:user._id}).populate('user','username _id')
-    })
-}
+const getByUser = async (req, res) => {
+  const username = req.params.username;
+  const user = await userSchema.findOne({ username });
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  return res.send({
+    data: await blogSchema
+      .find({ user: user._id })
+      .populate("user", "username _id"),
+  });
+};
 
-const getBlogsByUserId = async(req,res)=>{
-    const username = req.params.id;
-    const user = await userSchema.findById(username);
-    if (!user) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    return res.send({
-        data:await blogSchema.find({user:username}).populate('user','username _id')
-    })
-}
+const getBlogsByUserId = async (req, res) => {
+  const username = req.params.id;
+  const user = await userSchema.findById(username);
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  return res.send({
+    data: await blogSchema
+      .find({ user: username })
+      .populate("user", "username _id"),
+  });
+};
 
-const createBlog = async(req, res)=>{
-    await blogSchema.create({
-        title:req.body.title,
-        content:req.body.content,
-        user: req.body.user,
-        blogtypes: req.body.blogtypes
-    });
-    res.status(200).send("Blog Created")
-}
+const createBlog = async (req, res) => {
+  await blogSchema.create({
+    title: req.body.title,
+    content: req.body.content,
+    user: req.body.user,
+    blogtypes: req.body.blogtypes,
+  });
+  res.status(200).send("Blog Created");
+};
 
-const deleteBlog = async(req,res)=>{
-    const blogId= req.params.id;
-    await blogSchema.findByIdAndDelete(blogId);
-    res.status(200).send("Blog Deleted")
-}
+const deleteBlog = async (req, res) => {
+  const blogId = req.params.id;
+  await blogSchema.findByIdAndDelete(blogId);
+  res.status(200).send("Blog Deleted");
+};
 
-const editBlog = async(req,res)=>{
-    const blogId=req.params.id;
-    const updatedBlog={
-        title:req.body.title,
-        content:req.body.content
-    }
-    await blogSchema.findByIdAndUpdate(blogId,updatedBlog);
-    res.status(200).send("Blog Updated")
-}
+const editBlog = async (req, res) => {
+  const blogId = req.params.id;
+  const updatedBlog = {
+    title: req.body.title,
+    content: req.body.content,
+  };
+  await blogSchema.findByIdAndUpdate(blogId, updatedBlog);
+  res.status(200).send("Blog Updated");
+};
 
-const createBlogtype = async(req, res)=>{
-    await blogtypeschema.create({
-        name:req.body.name,
-    });
-    res.status(200).send("New Blogtype Created")
-}
+const createBlogtype = async (req, res) => {
+  await blogtypesSchema.create({
+    name: req.body.name,
+  });
+  res.status(200).send("New Blogtype Created");
+};
 
-const getBlogsByBlogTypeId = async(req, res)=>{
-    await blogSchema.find({
-        blogtypes: { 
-            $elemMatch: 
-            { $eq: req.params.id } 
-        } 
-    });
+const getBlogTypes = async (req, res) => {
+  return res.send({
+    data: await blogtypesSchema.find({}),
+  });
+};
 
-    res.status(200).send("New Blogtype Created")
-}
+const getBlogsByBlogTypeId = async (req, res) => {
+  await blogSchema.find({
+    blogtypes: {
+      $elemMatch: { $eq: req.params.id },
+    },
+  });
 
-
+  res.status(200).send("New Blogtype Created");
+};
 
 module.exports = {
-getAllBlog,
-getById,
-getByUser,
-createBlog,
-getBlogsByBlogTypeId,
-deleteBlog,
-editBlog,
-createBlogtype,
-}
-
+  getAllBlog,
+  getById,
+  getByUser,
+  createBlog,
+  getBlogsByBlogTypeId,
+  deleteBlog,
+  editBlog,
+  createBlogtype,
+  getBlogTypes,
+};
